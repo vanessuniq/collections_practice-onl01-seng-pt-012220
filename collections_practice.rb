99732fa4806b9d3bdb94abe216fc8d57df1faa28
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 23:17:31 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,23 @@
+def sort_array_asc intergers_array
+  intergers_array.sort
+end 
 
+def sort_array_desc intergers_array
+  intergers_array.sort {|a, b| b <=> a }
+end 
+
+def sort_array_char_count strings_array
+  strings_array.sort {|a, b| a.length <=> b.length}
+end 
+
+def swap_elements array 
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array 
+  array.reverse
+end 
+
+def kesha_maker
+end
